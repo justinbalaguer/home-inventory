78bdc86abd69b68e66e87bb4b8bc5c31c60bc2a9
Justin Balaguer <justinanthony.primary@gmail.com>
2020-08-02 14:34:23 +0800

    🔧Frontend RE:DO (CRUD for containers)

--- client/src/App.js
+++ client/src/App.js
@@ -7,143 +7,116 @@ import './styles/index.scss';
 const API_CONTAINER_URL = 'http://localhost:5000/api/containers/';
 
 const App = () => {
-  const [containers, setContainers] = useState([]);
-  const [containerInputs, setContainerInputs] = useState({
-    hidden: true,
-    action: ''
-  });
-  const [containerInfo, setContainerInfo] = useState({
+
+  /* CONTAINER */
+  const [container, setContainer] = useState([]);
+  const [info, setInfo] = useState({
     id: '',
     name: '',
-    color: '',
+    description: '',
     action: ''
   });
-  const {id,name,color} = containerInfo;
+  const {id,name,description} = info;
 
-  const getContainers = async() => {
-    const response = await axios.get(API_CONTAINER_URL);
+  //get containers
+  const getContainers = async () => {
+    const response = await axios.get(`${API_CONTAINER_URL}`);
     const data = response.data;
-    setContainers([...data]);
+    setContainer([...data]);
   }
+
+  //functions
+  const inputChange = (e) => {
+    const {name,value} = e.target;
+    setInfo({
+      ...info,
+      [name]: value
+    });
+  }
+
+  const handleAction = (id,name,description,action) => {
+    setInfo({
+      id: id,
+      name: name,
+      description: description,
+      action: action
+    });
+  }
+
+  const save = async () => {
+    if(info.action==='post') {
+      const response = await axios.post(`${API_CONTAINER_URL}`,info);
+      if(response.status===200) {
+        getContainers();
+      }
+    } else if(info.action==='put') {
+      const response = await axios.put(`${API_CONTAINER_URL}/${info.id}`,info);
+      if(response.status===200) {
+        getContainers();
+      }
+    } else if(info.action==='delete') {
+      const response = await axios.delete(`${API_CONTAINER_URL}/${info.id}`,info);
+      if(response.status===200) {
+        getContainers();
+      }
+    }
+  }
+
+  //container list
+  const containerList = container && container.map((data,i) => {
+    return(
+      <tr key={i}>
+        <td><Button block>{data.name}</Button></td>
+        <td>{data.description}</td>
+        <td><Button variant={'secondary'} block onClick={() => handleAction(data._id,data.name,data.description,'put')}>Edit</Button></td>
+        <td><Button variant={'danger'} block onClick={() => handleAction(data._id,'','','delete')}>Delete</Button></td>
+      </tr>
+    )
+  })
 
   useEffect(() => {
     getContainers();
   }, [])
 
-  const view = (id) => {
-    console.log(`view items from container ${id} `)
-  }
-
-  const handleAction = (id,name,color,action) => {
-    setContainerInfo({id: id, name: name, color: color, action: action});
-    setContainerInputs({hidden: false, action: action});
-  }
-
-  const inputChange = (e) => {
-    const {name,value} = e.target;
-    setContainerInfo({...containerInfo, [name]: value});
-  }
-
-  const save = async() => {
-    const id = containerInfo.id;
-    const name = containerInfo.name;
-    const color = containerInfo.color;
-    const action = containerInfo.action;
-    let obj = {
-      id: id,
-      name: name,
-      color: color
-    };
-    if(action==='put') {
-      const response = await axios.put(`${API_CONTAINER_URL}${id}`, obj);
-      if(response.status===200) {
-        getContainers();
-        setContainerInputs({hidden: true, action: ''});
-      }
-    } else if(action==='delete') {
-      const response = await axios.delete(`${API_CONTAINER_URL}${id}`, obj);
-      if(response.status===200) {
-        getContainers();
-        setContainerInputs({hidden: true, action: ''});
-      }
-    } else {
-      const response = await axios.post(`${API_CONTAINER_URL}`, obj);
-      if(response.status===200) {
-        getContainers();
-        setContainerInputs({hidden: true, action: ''});
-      }
-    }
-  }
-
-  const [searchKeyword,setSearchKeyword] = useState('');
-  const search = (e) => {
-    let keyword = e.target.value;
-    setSearchKeyword(keyword);
-  }
-
-  // eslint-disable-next-line
-  const containerData = containers.filter((data) => {
-    if(searchKeyword === '') {
-      return data;
-    } else if(data.name.toLowerCase().includes(searchKeyword) || (data.color.toLowerCase().includes(searchKeyword))){
-      return data;
-    }
-  }).map((data,i) => {
-    return(
-      <tr key={i}>
-        <td><Button type='button' block onClick={() => view(data._id)}>{data.name}</Button></td>
-        <td>{data.color}</td>
-        <td><Button type='button' variant='secondary' block onClick={() => handleAction(data._id,data.name,data.color,'put')}>Edit</Button></td>
-        <td><Button type='button' variant='danger' block onClick={() => handleAction(data._id,'','','delete')}>Delete</Button></td>
-      </tr>
-    )
-  })
-
   return (
     <Container className="App">
     <h1 className='text-center'>Home Inventory</h1>
-    <div className={`containers ${containerInputs.hidden === false && 'hidden'}`}>
+    <div>
       <div>
-      <Form.Control type='text' onChange={search} />
+      <Form.Control type='text' />
       <br/>
       </div>
-      <Button type='button' variant='success' block onClick={() => handleAction('','','','add')}>Add</Button>
+      <Button type='button' variant='success' block onClick={() => handleAction('','','','post')}>Add</Button>
       <Table striped bordered hover responsive>
         <thead>
           <tr>
-            <th>Container</th>
-            <th>Color</th>
+            <th>Name</th>
+            <th>Description</th>
             <th colSpan={2}>Action</th>
           </tr>
         </thead>
         <tbody>
-          {containerData}
+          {containerList}
         </tbody>
       </Table>
     </div>
-    {/* Edit Form */}
-    {containerInputs.hidden === false && (
-      <Form>
-        {containerInfo.action!=='delete' && (
-          <>
-          <Form.Group className>
-          <Form.Control type='text' id='id' name='id' onChange={inputChange} value={id} hidden={true} />
-            <Form.Label htmlFor='name'>Name</Form.Label>
-            <Form.Control type='text' id='name' name='name' onChange={inputChange} value={name} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor='color'>Color</Form.Label>
-            <Form.Control type='text' id='color' name='color' onChange={inputChange} value={color} />
-          </Form.Group>
-          </>
-        )}
+
+    {/* Form */}
+    <Form>
+    <Form.Group className>
+      <Form.Control type='text' id='id' name='id' onChange={inputChange} value={id} hidden={true} />
+        <Form.Label htmlFor='name'>Name</Form.Label>
+        <Form.Control type='text' id='name' name='name' onChange={inputChange} value={name} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor='description'>Description</Form.Label>
+        <Form.Control type='text' id='description' name='description' onChange={inputChange} value={description} />
+      </Form.Group>
         <Form.Group>
-          {containerInfo.action==='delete' && (<p>Are you sure you want to delete?</p>)}
-          <Button type='button' onClick={() => save(containerInputs.action)} >{containerInfo.action==='delete' ? ('Yes') : ('Save')}</Button>
-          <Button type='button' variant='danger' onClick={() => {setContainerInputs(true)}}>{containerInfo.action==='delete' ? ('No') : ('Cancel')}</Button>
+          <Button type='button' variant={`success`} onClick={save}>Save</Button>
+          <Button type='button' variant='danger'>Cancel</Button>
         </Form.Group>
       </Form>
-    )}
     </Container>
   );
 }
